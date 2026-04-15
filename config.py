e259a30e4358fb874bcd834a62494c702875c950
RamRamLagdar-hue <ankitgehlot120021@gmail.com>
2026-04-15 19:16:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
+# Licensed under the GNU General Public License v3.0.  
+# See LICENSE file in the repository root for full license text.
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# VPS --- FILL COOKIES 🍪 in """ ... """ 
+
+INST_COOKIES = """
+# wtite up here insta cookies
+"""
+
+YTUB_COOKIES = """
+# write here yt cookies
+"""
+
+API_ID = os.getenv("API_ID", "25255466")
+API_HASH = os.getenv("API_HASH", "aa797f1169fb6bbee3de4869d6b76165")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8606883284:AAH2Io7Iv58gCOmOSnvzD2NsYYFCmI1nJuY")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://RamRamLagdar:RamRamLagdar@cluster0.9rcnkyr.mongodb.net/?appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "8234931928").split())) # list seperated via space
+DB_NAME = os.getenv("DB_NAME", "Cluster0")
+STRING = os.getenv("STRING", None) # optional
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002542800168")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002542800168")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
+INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "50000"))
